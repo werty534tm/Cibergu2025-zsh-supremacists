a503b5665358489b4a7a34f2084b854ce1bc8e68
Raymer753 <garciaizan56@hotmail.com>
2025-06-03 11:53:22 +0000

    funciona todo

--- servicios/scripts/arduino.sh
+++ servicios/scripts/arduino.sh
@@ -1,20 +1,57 @@
 #!/bin/env zsh
 
-ip_telegraph=172.22.0.5:8080/submit_air_quality
+# URL corregida con http://
+ip_telegraph="http://172.22.0.5:8080/submit_air_quality"
 intervalo=10
+
+# Valores para los tags (puedes cambiarlos o hacerlos dinámicos)
+SENSOR_ID_VAL="script_zsh_sensor01"
+LOCATION_VAL="lab_simulacion"
+
+echo "Iniciando envío de datos a Telegraf en $ip_telegraph cada $intervalo segundos..."
+echo "Presiona Ctrl+C para detener."
 
 while :
 do
-  co2=$(( $RANDOM % 15 ))
-  co=$(( $RANDOM % 8 ))
-  metales=$(( $RANDOM % 120 ))
-  
-  
-  echo $co2
-  echo $co
-  echo $metales
+  # Generación de datos
+  co2_val=$(( $RANDOM % 15 + 400 )) # Ejemplo: CO2 entre 400 y 414
+  co_val=$(( ($RANDOM % 80) / 10.0 )) # Ejemplo: CO entre 0.0 y 7.9 (requiere zsh para flotantes así o usa otra técnica)
+  metales_val=$(( $RANDOM % 120 ))
+  timestamp_val=$(date +%s)
 
-  curl -X POST -H "Content-Type: application/json" -d '{"salida": {"co2": $co2, "co":$co, "metales": $metales, "timestamp": '$(date +%s)'}}' $ip_telegraph
+  echo "----------------------------------------"
+  echo "Valores generados:"
+  echo "  Sensor ID: $SENSOR_ID_VAL"
+  echo "  Location:  $LOCATION_VAL"
+  echo "  CO2:       $co2_val ppm"
+  echo "  CO:        $co_val ppm"
+  echo "  Metales:   $metales_val units" # Ajusta las unidades según corresponda
+  echo "  Timestamp: $timestamp_val"
+
+  JSON_PAYLOAD="{
+    \"sensor_id\": \"$SENSOR_ID_VAL\",
+    \"location\": \"$LOCATION_VAL\",
+    \"co\": $co_val,
+    \"co2\": $co2_val,
+    \"metales\": $metales_val,
+    \"timestamp\": $timestamp_val
+  }"
+
+  echo "JSON a enviar:"
+  echo "$JSON_PAYLOAD"
+  echo "Enviando a $ip_telegraph ..."
+
+  curl -v -sS --fail -X POST \
+    -H "Content-Type: application/json" \
+    -d "$JSON_PAYLOAD" \
+    "$ip_telegraph"
+
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    echo "Datos enviados correctamente a Telegraf."
+  else
+    echo "ERROR: curl falló con código de salida $exit_code."
+  fi
 
   sleep $intervalo
 done
